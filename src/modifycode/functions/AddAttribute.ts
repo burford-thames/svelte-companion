50fced9035f844f7f47b1635764bd11abd15231b
burford thames <burford_thames@protonmail.com>
2023-02-19 21:50:01 +0700

    add attribute's values

--- src/modifycode/functions/AddAttribute.ts
+++ src/modifycode/functions/AddAttribute.ts
@@ -1,13 +1,12 @@
 import * as vscode from "vscode";
 import { TreeItem } from "../../types/LayoutTreeTypes";
 import { getEditorSpacing, getElementSpacing, getLastAttributePosition } from "../../utils/GettingPositionUtil";
-import { globalAttributes, specificAttributes } from "../attributeData";
+import { globalAttributes, specificAttributes, values } from "../attributeData";
 
 export function addAttribute(item: TreeItem): void {
-  const insertPosition = getLastAttributePosition(item);
-
   // Create quick pick
   const quickPick = vscode.window.createQuickPick();
+  quickPick.title = "Select an attribute";
   const specificAttributesForThisElement = specificAttributes.get(item.label as string) ?? [];
   quickPick.items = [...specificAttributesForThisElement, ...globalAttributes];
 
@@ -17,26 +16,76 @@ export function addAttribute(item: TreeItem): void {
   // On selection
   quickPick.onDidChangeSelection((selection) => {
     const selectedAttribute = selection[0].label;
-    let attribute: string;
-
-    // If the element and the attribute are on the same line, add a space before the attribute
-    const itemPosition = item.start ?? new vscode.Position(0, 0);
-    if (itemPosition.line === insertPosition.line) {
-      attribute = ` ${selectedAttribute}=""`;
-    } else {
-      // If the element and the attribute are on different lines, add a new line before the attribute
-      const editorSpacing = getEditorSpacing();
-      const elementSpacing = getElementSpacing(item);
-      attribute = `\n${elementSpacing + editorSpacing}${selectedAttribute}=""`;
-    }
-
-    // Insert attribute
-    vscode.window.activeTextEditor?.edit((editBuilder) => {
-      editBuilder.insert(insertPosition, attribute);
-    });
-    vscode.window.activeTextEditor?.document.save();
+    promptForValueAndInsertAttribute(selectedAttribute, item);
 
     // Hide quick pick
     quickPick.hide();
   });
+}
+
+function promptForValueAndInsertAttribute(selectedAttribute: string, item: TreeItem): void {
+  const possibleValues = values.get(selectedAttribute);
+  let attributeString: string;
+
+  switch (possibleValues?.type) {
+    case "empty":
+      attributeString = selectedAttribute;
+      insertAttributeString(item, attributeString);
+      break;
+    case "input":
+      const inputBox = vscode.window.createInputBox();
+      inputBox.title = "Enter a value";
+      inputBox.show();
+      inputBox.onDidAccept(() => {
+        attributeString = `${selectedAttribute}="${inputBox.value}"`;
+        insertAttributeString(item, attributeString);
+        inputBox.hide();
+      });
+      break;
+    case "single-select":
+      const quickPick2 = vscode.window.createQuickPick();
+      quickPick2.title = "Select a value";
+      quickPick2.items = possibleValues.values!;
+      quickPick2.show();
+      quickPick2.onDidChangeSelection((selection2) => {
+        attributeString = `${selectedAttribute}="${selection2[0].label}"`;
+        insertAttributeString(item, attributeString);
+        quickPick2.hide();
+      });
+      break;
+    case "multi-select":
+      const quickPick3 = vscode.window.createQuickPick();
+      quickPick3.title = "Select values";
+      quickPick3.items = possibleValues.values!;
+      quickPick3.canSelectMany = true;
+      quickPick3.show();
+      quickPick3.onDidAccept(() => {
+        attributeString = `${selectedAttribute}="${quickPick3.selectedItems.map((item) => item.label).join(" ")}"`;
+        insertAttributeString(item, attributeString);
+        quickPick3.hide();
+      });
+      break;
+  }
+}
+
+function insertAttributeString(item: TreeItem, attributeString: string): void {
+  const insertPosition = getLastAttributePosition(item);
+
+  // If the element and the attribute are on the same line, add a space before the attribute
+  const itemPosition = item.start ?? new vscode.Position(0, 0);
+  let paddedAttribute: string;
+  if (itemPosition.line === insertPosition.line) {
+    paddedAttribute = ` ${attributeString}`;
+  } else {
+    // If the element and the attribute are on different lines, add a new line before the attribute
+    const editorSpacing = getEditorSpacing();
+    const elementSpacing = getElementSpacing(item);
+    paddedAttribute = `\n${elementSpacing + editorSpacing}${attributeString}`;
+  }
+
+  // Insert attribute
+  vscode.window.activeTextEditor?.edit((editBuilder) => {
+    editBuilder.insert(insertPosition, paddedAttribute);
+  });
+  vscode.window.activeTextEditor?.document.save();
 }
